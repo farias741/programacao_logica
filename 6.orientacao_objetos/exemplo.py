7ded438cdd9cd08914bb8e66ae42ea5721ba40e8
farias741 <deborafarias297@gmail.com>
2025-02-13 23:46:56 -0300

    Orientação a Objetos

---
+++ 6.orientacao_objetos/exemplo.py
@@ -0,0 +1,11 @@
+class aluno:
+    def __init__(self, matricula, nome):
+        self.matricula = matricula
+        self.nome = nome
+    def matricular(self):
+        print("Aluno: ", self.nome, "matriculado")
+
+jose = aluno("123", "José da Silva")
+print(jose.matricula)
+print(jose.nome)
+jose.matricular()
